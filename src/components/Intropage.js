fdfc6fb65e7879bf25243c349292a8a70e3dc0a1
savitha A <80628303+savi1248@users.noreply.github.com>
2024-03-23 11:19:06 +0530

    chza

--- src/components/Intropage.js
+++ src/components/Intropage.js
@@ -6,30 +6,29 @@ import profileimg from '../assets/photo.jpeg'
 const Intropage = () => {
     return (
         <main className='main'>
-                   
             <div className='main-container'>
-                
+
                 <div className='main-content'>
                     <p className='main-subtitle'>Hi,</p>
-                        <h1 className='main-title'>
-                            I'm <span>savitha<br /></span>Web Developer
-                        </h1>
-                        <p className='description'>
-                        Experienced in testing domains with a passion for frontend development. I am seeking to transition into a front-end developer role. I am proficient in HTML, CSS, Bootstrap, JavaScript (including ES6), and React JS. I bring valuable internship experience for creating user-friendly and responsive web interfaces.    
+                    <h1 className='main-title'>
+                        I'm <span>savitha<br /></span>Web Developer
+                    </h1>
+                    <p className='description'>
+                        Experienced in testing domains with a passion for frontend development. I am seeking to transition into a front-end developer role. I am proficient in HTML, CSS, Bootstrap, JavaScript (including ES6), and React JS. I bring valuable internship experience for creating user-friendly and responsive web interfaces.
 
-                        </p>
-                        <div >
-                            
-                            <a href='https://drive.google.com/file/d/1yXs_0N5dDA5bSl-5L39nXcigIJCy_7Nk/view?usp=drivesdk' target='_blank'   className='btn'>CV Download</a>
-                            
-                            <Link to='/about' className='btn btn-light'> About me</Link>
-                        </div>
+                    </p>
+                    <div >
+
+                        <a href='https://drive.google.com/file/d/1-F5dVwjh2GeivRbcQppw5N1Eex5SWJRY/view?usp=drivesdk' target='_blank' className='btn'>CV Download</a>
+
+                        <Link to='/about' className='btn btn-light'> About me</Link>
                     </div>
-                     <div className='main-image'>
-                          <img src={profileimg} alt='profiles' className='main-img'/>
-                     </div>
                 </div>
-            
+                <div className='main-image'>
+                    <img src={profileimg} alt='profiles' className='main-img' />
+                </div>
+            </div>
+
         </main>
     )
 }
